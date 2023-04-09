2f4ba230e171b729bc958f9c90b67a204aa65f92
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-09 03:23:58 -0300

    Created filem system and main template

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,9 +1,26 @@
 import './sass/App.scss'
+import { Link } from 'react-router-dom'
+import Weather from './components/Weather'
+
+// Links:
+// https://rapidapi.com/weatherapi/api/weatherapi-com/
+// https://www.weatherapi.com/docs/
+// https://www.weatherapi.com/docs/weather_conditions.json
+// https://dribbble.com/shots/21084146-Weather-app
 
 function App() {
 	return (
 		<div className="App">
-			<h1>First Commit</h1>
+			<header>
+				<Link to={"http://tomas-alberdi.com"} target='_blank'><img src="/favicon.ico" alt="Link a Portfolio" /></Link>
+			</header>
+			<main>
+				<section className="search">
+					<input type="text" name="city" id="city" placeholder='Search a city' />
+					<button><i className="fa-solid fa-magnifying-glass"></i></button>
+				</section>
+				<Weather />
+			</main>
 		</div>
 	)
 }
---
+++ weather-app/src/components/Weather.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Weather = () => {
+    return (
+        <section className="Weather">
+            <h1>test</h1>
+        </section>
+    )
+}
+
+export default Weather
