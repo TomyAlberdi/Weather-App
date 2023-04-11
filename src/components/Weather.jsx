bc96a88ef6b73411b69422a9e26a0f2688c9a844
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-04-10 23:28:54 -0300

    Completed basic layout

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -18,6 +18,25 @@ const Weather = ({LoadingData,Data}) => {
                     <h1>{Data.location.name}, {Data.location.country}</h1>
                     <h3>At {Data.location.localtime.substring(11,16)}</h3>
                 </article>
+                <article className="icon div2">
+                    <h3>{Data.current.condition.text}</h3>
+                    <img src={
+                        Data.current.condition.icon.substring(28)
+                    } />
+                </article>
+                <article className="temp div3">
+                    <h2>{Data.current.temp_c} <span>C</span></h2>
+                </article>
+                <article className="wind div4">
+                    <i className="fa-solid fa-wind"></i>
+                    <h3>{Data.current.wind_kph} <span>km/h</span></h3>
+                </article>
+                <article className="humid div5">
+                    <h2>himidity</h2>
+                </article>
+                <article className="uv div6">
+                    <h2>uv rays</h2>
+                </article>
             </section>
         )
     }
